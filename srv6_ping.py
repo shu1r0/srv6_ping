82920190f33358a9f68a765db8cf1a69164d034a
shu1r0 <shimatani180231n@gmail.com>
2022-12-15 00:57:37 +0900

    update

--- srv6_ping.py
+++ srv6_ping.py
@@ -6,7 +6,8 @@ def get_args(description=None):
     parser = argparse.ArgumentParser(description=description)
 
     parser.add_argument('-d', '--destination', help="destination")
-    parser.add_argument('-s', '--segs', default="", help="segment_list. e.g.) fd0a::,fd0b::,fd0c::")
+    parser.add_argument('-s', '--segs', default="", help="segment_list. eg.) fd0a::,fd0b::,fd0c::")
+    parser.add_argument('-t', '--timeout', default=3, help="timeout")
 
     args = parser.parse_args()
     return args
@@ -14,4 +15,4 @@ def get_args(description=None):
 
 if __name__ == '__main__':
     args = get_args()
-    ping_and_show(args.destination, args.segs.split(","))
+    ping_and_show(args.destination, args.segs.split(","), timeout=args.timeout)
--- srv6_ping/ping.py
+++ srv6_ping/ping.py
@@ -1,50 +1,52 @@
 import time
 from typing import List, Optional
 
-from scapy.all import IPv6, ICMPv6EchoRequest, IPv6ExtHdrSegmentRouting, ICMPv6EchoReply, sr1
+from scapy.all import IPv6, ICMPv6EchoRequest, IPv6ExtHdrSegmentRouting, ICMPv6EchoReply, sr1, sr
 from scapy.layers.inet6 import ICMPv6EchoReply, ICMPv6DestUnreach, ICMPv6PacketTooBig, ICMPv6TimeExceeded, ICMPv6ParamProblem
 
 
-def ping_and_show(dst: str, segs: List[str] = None, timeout: int = 1):
+def ping_and_show(dst: str, segs: List[str] = None, timeout: int = 3):
     try:
         while True:
             result = ping1(dst, segs, timeout)
             if result:
-                print("%s code=%d from=%s hlim=%d rtt=%d" % \
+                print("%s code=%d from=%s hlim=%d rtt=%f" % \
                       (result["msg"], result["code"], result["rep_src"], result["hlim"], result["rtt"]))
+            else:
+                print("Timeout.")
             time.sleep(1)
     except KeyboardInterrupt:
-        print("end.")
+        print("")
 
 
-def ping1(dst: str, segs: List[str] = None, timeout: int = 1) -> Optional[dict]:
+def ping1(dst: str, segs: List[str] = None, timeout: int = 3, verbose=1) -> Optional[dict]:
     packet = get_icmp_packet(dst, segs)
     start = time.time()
-    rep = sr1(packet, timeout=timeout)
+    rep = sr1(packet, timeout=timeout, verbose=verbose)
     if rep:
         end = time.time()
         result = {}
         result["hlim"] = rep[IPv6].hlim
         result["rep_src"] = rep[IPv6].src
-        result["rtt"] = end - start
+        result["rtt"] = (end - start)*1000
 
         code = -1
         msg = "UNKOWN MESSAGE"
         if ICMPv6EchoReply in rep:
             code = rep[ICMPv6EchoReply].code
-            msg = "ICMPv6EchoReply"
+            msg = "EchoReply"
         elif ICMPv6DestUnreach in rep:
             code = rep[ICMPv6DestUnreach].code
-            msg = "ICMPv6DestUnreach"
+            msg = "DestUnreach"
         elif ICMPv6PacketTooBig in rep:
             code = rep[ICMPv6PacketTooBig].code
-            msg = "ICMPv6PacketTooBig"
+            msg = "PacketTooBig"
         elif ICMPv6TimeExceeded in rep:
             code = rep[ICMPv6TimeExceeded].code
-            msg = "ICMPv6TimeExceeded"
+            msg = "TimeExceeded"
         elif ICMPv6ParamProblem in rep:
             code = rep[ICMPv6ParamProblem].code
-            msg = "ICMPv6ParamProblem"
+            msg = "ParamProblem"
         result["code"] = code
         result["msg"] = msg
 
@@ -54,9 +56,10 @@ def ping1(dst: str, segs: List[str] = None, timeout: int = 1) -> Optional[dict]:
 
 
 def get_icmp_packet(dst: str, segs: List[str] = None):
-    if segs and len(segs) > 0:
-        segs.insert(0, dst)
-        return IPv6(dst=segs[0])/IPv6ExtHdrSegmentRouting(addresses=segs)/ICMPv6EchoRequest(data="x"*32)
+    if segs and len(segs) > 0 and segs[0] != "":
+        s = segs[::-1]
+        s.insert(0, dst)
+        return IPv6(dst=s[-1])/IPv6ExtHdrSegmentRouting(addresses=s)/ICMPv6EchoRequest(data="x"*32)
     else:
         return IPv6(dst=dst)/ICMPv6EchoRequest(data="x"*32)
 
