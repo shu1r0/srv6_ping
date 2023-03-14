04d76430551a0c5e1d633b5ab671c746c6bc1bb3
shu1r0 <shu1r0.net@gmail.com>
2023-03-14 15:38:33 +0900

    fix workflows

--- test_srv6_ping.sh
+++ test_srv6_ping.sh
@@ -2,6 +2,8 @@
 
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run with sudo" ; exit 1 ; fi
 
+set -e
+
 ./tests/netns_network_examples/simple/linear_4hosts.sh -c 
 ip netns exec ns4 ip -6 route add 2001:db8:10::1/128 encap seg6 mode inline segs 2001:db8:30::1,2001:db8:20::1 dev ns4_ns3
 ip netns exec ns1 python3 -m unittest discover tests/
